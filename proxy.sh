31230638929ea8c1f48a3f53acae2354b28791e6
冯文轩 <moxixi>
2022-07-22 23:32:34 +0800

    feat: update

---
+++ proxy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git config --global https.proxy http://127.0.0.1:1087
+
+git config --global https.proxy https://127.0.0.1:1087
+
+git config --global --unset http.proxy
+
+git config --global --unset https.proxy
+
+
+npm config delete proxy
